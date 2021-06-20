b6b41d32cc46a7b3a703708f6b986b1b3bdf38a3
Chathura <Chathura@grubtech.com>
2021-06-21 00:09:41 +0530

    merge master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {ProductComponent} from './components/product/product.component';
-import {CartComponent} from './components/cart/cart.component';
-import {ProductItemComponent} from './components/product-item/product-item.component';
-import {ProductContainerComponent} from './components/product-container/product-container.component';
-import {NavComponent} from './components/nav/nav.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {eShopInterceptor} from "./services/e-shop-interceptor.service";
-import {AngularMaterialModule} from "./angular-material.module";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {UsernamePipe} from "./pipes/username.pipe";
-import {ReactiveFormsModule} from "@angular/forms";
-import {UnauthorizedComponent} from './components/unautorized/unauthorized.component';
-import {WelcomeComponent} from './components/welcome/welcome.component';
-import {NgxSpinnerModule} from "ngx-spinner";
-import {ScrollDirective} from "./directive/scroll.directive";
-import {LimitChars} from "./pipes/limt-chars";
-import {NumberOnlyDirective} from "./directive/number-only.directive";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ProductComponent } from './components/admin/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductContainerComponent } from './components/product-container/product-container.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { eShopInterceptor } from "./services/e-shop-interceptor.service";
+import { AngularMaterialModule } from "./angular-material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsernamePipe } from "./pipes/username.pipe";
+import { ReactiveFormsModule } from "@angular/forms";
+import { UnauthorizedComponent } from './components/unautorized/unauthorized.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { ScrollDirective } from "./directive/scroll.directive";
+import { LimitChars } from "./pipes/limt-chars";
+import { NumberOnlyDirective } from "./directive/number-only.directive";
 
 @NgModule({
   declarations: [
@@ -54,7 +54,7 @@ import {NumberOnlyDirective} from "./directive/number-only.directive";
     BrowserAnimationsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: eShopInterceptor, multi: true}],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: eShopInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/components/admin/product/product.component.spec.ts
@@ -0,0 +1,53 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ProductComponent} from './product.component';
+import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {ProductService} from "../../../services/product.service";
+import {MatSort, MatSortModule} from "@angular/material/sort";
+import {MatTableModule} from "@angular/material/table";
+import {By} from "@angular/platform-browser";
+import {ReactiveFormsModule} from "@angular/forms";
+import {Overlay} from "@angular/cdk/overlay";
+
+describe('ProductComponent', () => {
+  let component: ProductComponent;
+  let fixture: ComponentFixture<ProductComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProductComponent],
+      imports: [
+        MatSnackBarModule,
+        MatTableModule,
+        MatSortModule,
+        ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ProductService, MatSnackBar, Overlay ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('initially sets up sorting', () => {
+    fixture.detectChanges();
+    const sort = component.dataSource.sort;
+    expect(sort).toBeInstanceOf(MatSort);
+  });
+
+  it('should call save() method on form submit', () => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const getForm = fixture.debugElement.query(By.css('#formID'));
+    expect(getForm.triggerEventHandler('submit', compiled)).toBeUndefined();
+  });
+});
