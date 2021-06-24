daf4e824449713068563c35636dfdbd6a9e8fdfc
Chathura <Chathura@grubtech.com>
2021-06-24 15:24:42 +0530

    tests

--- src/app/components/picture/picture.component.spec.ts
+++ src/app/components/picture/picture.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PictureComponent } from './picture.component';
+import {By} from "@angular/platform-browser";
+import {DebugElement} from "@angular/core";
 
-describe('PictureComponent', () => {
+fdescribe('PictureComponent', () => {
   let component: PictureComponent;
   let fixture: ComponentFixture<PictureComponent>;
+  let imgEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,11 +18,18 @@ describe('PictureComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PictureComponent);
+    imgEl = fixture.debugElement.query(By.css('img'));
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('image should be visible', () => {
+    component.url = 'http://placeimg.com/640/480/cats';
+    fixture.detectChanges();
+    expect(imgEl.nativeElement.src).toBe('http://placeimg.com/640/480/cats');
   });
 });
